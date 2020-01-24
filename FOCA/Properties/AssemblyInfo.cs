0473198073e864c1d92f54cb11b5384f1afad526
Angelystor <kurayami.tenshi@gmail.com>
2020-01-24 08:13:45 +0800

    Revert "Renamed project to QUAKA"
    This reverts commit 41db5ea27bdf3acfd9280318edda2381fc193843.

--- FOCA/Properties/AssemblyInfo.cs
+++ FOCA/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QUAKA (Open Source)")]
+[assembly: AssemblyTitle("FOCA (Open Source)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("elevenpaths")]
-[assembly: AssemblyProduct("QUAKA Open Source")]
+[assembly: AssemblyProduct("FOCA Open Source")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
